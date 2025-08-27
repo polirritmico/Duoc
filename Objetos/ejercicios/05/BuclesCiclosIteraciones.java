6315be05e04516aa656d4e3ca55b23282766b87b
Eduardo Bray <ejbray@uc.cl>
2025-08-26 22:04:48 -0400

    objetos: update class 2025-08-26

---
+++ Objetos/ejercicios/05/BuclesCiclosIteraciones.java
@@ -0,0 +1,29 @@
+public class BuclesCiclosIteraciones {
+    public static void main(String[] args) {
+        // for: índice y condición
+        for (int i = 0; i < 10; i++) {
+            // ...
+        }
+
+        // post-incremento
+        int i = 10;
+        int a = i++; // a → 10; i → 11;
+        int b = ++i; // b → 12; i → 12;
+
+        // while
+        int condicion = 15;
+        while (condicion < 18) {
+            condicion++;
+        }
+
+        // do-while
+        condicion = 15;
+        do {
+            // ...
+        } while (condicion < 18);
+
+        // --------------------------------------------------------------------
+
+
+    }
+}
