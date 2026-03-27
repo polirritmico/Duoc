4a2759d4803600a2ff5b7cc409ff75b44dd6f6ab
Eduardo Bray <ejbray@uc.cl>
2026-03-26 21:06:41 -0300

    fullstack: add MicroservicioFiestas

---
+++ Fullstack1/code/MicroservicioFiestas/src/main/java/cl/duoc/MicroservicioFiestas/MicroservicioFiestasApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.MicroservicioFiestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioFiestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioFiestasApplication.class, args);
+	}
+
+}
---
+++ Fullstack1/code/MicroservicioFiestas/src/test/java/cl/duoc/MicroservicioFiestas/MicroservicioFiestasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.MicroservicioFiestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioFiestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
